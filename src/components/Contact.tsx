bcb15bbbc55242e46d318bbcee9b3e697a3ed5c1
unknown <adarshkamble01@gmail.com>
2024-12-03 00:22:56 +0530

    contact section

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -11,9 +11,30 @@ export function Contact() {
           TOGETHER
         </div>
       </h1>
-      <div className="mt-10 flex gap-10 w-full">
-        <InputTag header={"Name"} />
-        <InputTag header={"Email"} />
+      <div className="mt-10 flex gap-5 w-full">
+        <InputTag header={"Name"} desc="Your Name" />
+        <InputTag header={"Email"} desc="Your@email.com" />
+      </div>
+      <div className="mt-5">
+        {" "}
+        <div className="text-neutral-400 text-sm mb-2.5 font-light">
+          Message
+        </div>
+        <div className=" ">
+          <textarea
+            placeholder="Message"
+            className={`h-28  w-full min-w-[300px]  rounded-xl bg-[#353334]     pt-3  text-base pl-4 text-white   focus:outline-none focus:ring-2 focus:ring-lime-400   transition  duration-300  `}
+          />
+        </div>
+      </div>
+      <div className="mt-5 flex justify-end">
+        <button
+          className="bg-lime-400 
+         hover:bg-lime-500  transition-all  duration-300 ease-in-out 
+         text-sm p-2.5 rounded-xl px-5 font-medium "
+        >
+          Submit
+        </button>
       </div>
     </div>
   );
--- src/components/InputTag.tsx
+++ src/components/InputTag.tsx
@@ -1,15 +1,17 @@
 interface propType {
   header: string;
+  desc: string;
 }
 
-export function InputTag({ header }: propType) {
+export function InputTag({ header, desc }: propType) {
   return (
     <div>
-      <div className="text-neutral-400 text-sm mb-2.5">{header}</div>
+      <div className="text-neutral-400 text-sm mb-2.5 font-light">{header}</div>
       <div className=" ">
         <input
           type="text"
-          className={`h-12 w-full  rounded-xl bg-stone-700     text-base pl-5 text-neutral-400   focus:outline-none focus:ring-2 focus:ring-lime-400   transition-all duration-300  `}
+          placeholder={desc}
+          className={`h-10  w-full min-w-[330px]  rounded-xl bg-[#353334]     text-sm pl-4 text-white   focus:outline-none focus:ring-2 focus:ring-lime-400   transition  duration-300  `}
         />
       </div>
     </div>
